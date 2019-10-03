1af8fdb505c332890d6225de6ee3dc07f6573237
yordyjc <yordyjc.chura@gmail.com>
2019-10-03 17:04:27 -0500

    vista muñeco

---
+++ resources/views/admin/include/threejs.blade.php
@@ -0,0 +1,2 @@
+{!! Html::script('resources/admin/bower_components/jquery/js/jquery.min.js') !!}
+{!! Html::script('resources/admin/bower_components/bootstrap/js/bootstrap.min.js') !!}
---
+++ resources/views/user/js.blade.php
@@ -0,0 +1,6 @@
+{!! Html::script('resources/admin/assets/js/threejs-loader/three.js') !!}
+{!! Html::script('resources/admin/assets/js/threejs-loader/three.min.js') !!}
+{!! Html::script('resources/admin/assets/js/threejs-loader/Orbit.js') !!}
+{!! Html::script('resources/admin/assets/js/threejs-loader/GLTFLoader.js') !!}
+{!! Html::script('resources/admin/assets/js/threejs-loader/data.gui.js') !!}
+@yield('js')
