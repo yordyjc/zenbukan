b33c63ab3db351c15529b24571f35f004d3bedf1
Walter Calizaya <waltercalizaya@hotmail.com>
2019-10-12 19:17:54 -0500

    Laravel excel incluido en el proyecto

--- config/app.php
+++ config/app.php
@@ -165,6 +165,7 @@ return [
         Laraveles\Spanish\SpanishServiceProvider::class,
         Orangehill\Iseed\IseedServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
 
     ],
 
@@ -218,6 +219,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
 
     ],
 
