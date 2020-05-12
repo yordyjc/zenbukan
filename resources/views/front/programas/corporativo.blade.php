24627b52ca8414dc665443adb1f4c978ab1758b0
yordyjc <yordyjc.chura@gmail.com>
2020-05-12 00:06:05 -0500

    pagianas terminadas

--- resources/views/front/programas/corporativo.blade.php
+++ resources/views/front/programas/corporativo.blade.php
@@ -5,119 +5,95 @@ Corporativo
 @endsection
 
 @section('contenido')
-<section class="xs-bg-cover" style="background: url({{ $fondo ? $fondo : '/resources/img/fondos/default.jpg' }}) center; background-size: cover;">
+<audio src="/resources/audio/bebes.mp3" autoplay loop></audio>
+<section>
+    <div style="height=100%; width=900px;">
+        <img src="/resources/img/index/corporativo/portada.png"alt ="" width=100%;>
+    </div>
+</section>
+
+<section class="xs-pt-xs xs-pb-sm">
     <div class="container">
         <div class="row">
-            <div class="col-md-12">
-                <div class="xs-banner-area">
-                    <h1 class="xs-banner-title">@yield('titulo')</h1>
+            <div class="col-md-6">
+                <div class="xs-team">
+                    <div>
+                        <img src="resources/img/index/corporativo/co3.png" alt="" />
+                    </div>
+                    <div class="xs-team-content">
+                        <h5>
+                            CLASES CORPORATIVAS 
+                        </h5>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="xs-team">
+                    <div>
+                        <img src="resources/img/index/corporativo/co2.png" alt="" />
+                    </div>
+                    <div class="xs-team-content">
+                        <h5>
+                            INTEGRACIÓN DE EQUIPOS - JORNADAS DE ENTRENAMIENTO 
+                        </h5>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</section>
-<section class="xs-black-white xs-section-padding-xs">
 
-    <section class="xs-section-padding" data-scrollax-parent="true">
-        <div class="container">
-            <div class="xs-help xs-strength">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="xs-video-wraper">
-                            <img src="resources/img/index/corporativo/corporativo.png" alt="" />
-                            
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        </br>
-                        </br>
-                        </br>
-                        <div class="xs-pl-15 xs-pr-15">
-                            <div class="xs-section-heading text-left">
-                                <h2>
-                                    Entrenamiento corporativo
-                                </h2>
-                            </div>
-                                <p>Trabajadores sanos y felices producirán mejores resultados</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- .xs-help END -->
-        </div>
-        <!-- .container END -->
-    </section>
 </section>
-<section
-    class="xs-section-padding-lg xs-bg-cover parallaxie"
-    style=" background-image: url('resources/img/index/corporativo/corporativo-frase.png'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed; background-position: center 77.32px;"
->
+
+<section style="background-color:#0083AD;">
     <div class="container">
-        <div class="xs-help xs-help-light">
+        <div>
             <div class="row">
-                <div class="col-lg-6"style="color:#fff;">
-                    <div class="xs-section-heading">
-                        <h2 style="color:#fff;">
-                            BENEFICIOS
-                        </h2>
+                <div class="col-lg-8 mx-auto">
+                    </br>
+                    </br>
+                    </br>
+                    <div class="xs-section-heading text-center"style="color:#ffffff">
+                        <h2 style="color:#ffffff">Importancia de la gimnasia laboral</h2>
+                        <p>La gimnasia laboral o también conocida como pausas activas, consiste en la ejecución de ejercicios físicos correctamente estructurados que actúan de manera terapéutica y preventiva, propiciando el bienestar general de sus trabajadores.</p>
                     </div>
-                    <ul class="icons">
-                        <li><i class="fa fa-check-circle"></i> Disminuye el estrés laboral.</li>
-                        <li><i class="fa fa-check-circle"></i> Favorece el cambio de posturas y rutina.</li>
-                        <li><i class="fa fa-check-circle"></i> Libera estrés articular y muscular.</li>
-                        <li><i class="fa fa-check-circle"></i> Estimula y favorece la circulación.</li>
-                        <li><i class="fa fa-check-circle"></i> Mejora la postura.</li>
-                        <li><i class="fa fa-check-circle"></i> Favorece la autoestima y capacidad de concentración.</li>
-                        <li><i class="fa fa-check-circle"></i> Motiva y mejora las relaciones interpersonales y promueve la integración social.</li>
-                        <li><i class="fa fa-check-circle"></i> Mejora el desempeño laboral.</li>
-                    </ul>
-                    
                 </div>
             </div>
         </div>
-        <!-- .xs-help END -->
     </div>
-    <!-- .container END -->
 </section>
-<section class="xs-section-padding">
+</br>
+<section class="xs-pt-xs xs-pb-sm">
     <div class="container">
+    <div class="xs-help xs-strength">
         <div class="row">
-            <div class="col-lg-8 mx-auto">
-                <div class="xs-section-heading text-center">
-                    <h2>IMPORTANCIA DE LA GIMNASIA LABORAL</h2>
-                    <p>La gimnasia laboral o también conocida como pausas activas, consiste en la ejecución de ejercicios físicos correctamente estructurados que actúan de manera terapéutica y preventiva, propiciando el bienestar general de sus trabajadores.</p>
+            <div class="col-lg-6 text-center">
+                <img src="resources/img/index/corporativo/co1.png" alt=""/>
+            </div>
+            <div class="col-lg-6 text-center">
+
+                <div class="xs-pl-15 xs-pr-15">
+                    <h2>
+                        Beneficios
+                    </h2>
+                    <ul>
+                        <li>Disminuye el estrés laboral</li>
+                        <li>Favorece el cambio de posturas y rutina.</li>
+                        <li>Libera estrés articular y muscular.</li>
+                        <li>Estimula y favorece la circulación.</li>
+                        <li>Mejora la postura.</li>
+                        <li>Favorece la autoestima y capacidad de concentración.</li>
+                        <li>Motiva y mejora las relaciones interpersonales y promueve la integración social.</li>
+                        <li>Mejora el desempeño laboral.</li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <!-- .row END -->
     </div>
-    <!-- .container END -->
+    <!-- .xs-help END -->
+    </div>
+
 </section>
 
-<div class="owl-carousel owl-theme xs-service-info owl-loaded owl-drag">
-    <div class="owl-stage-outer">
-        <div class="owl-stage" style="transform: translate3d(-1801px, 0px, 0px); transition: all 0.25s ease 0s; width: 3089px;">
-            <div class="owl-item" style="width: 257.4px;">
-                <div class="item"style="height=200px;">
-                    <img src="assets/images/corporativo/co1.png" alt="" />
-                </div>
-            </div>
-            <div class="owl-item" style="width: 257.4px;">
-                <div class="item"style="height=200px;">
-                    <img src="resources/img/index/corporativo/co2.png" alt="" />
-                </div>
-            </div>
-            <div class="owl-item" style="width: 257.4px;">
-                <div class="item"style="height=200px;">
-                    <img src="resources/img/index/corporativo/co3.png" alt="" />
-                </div>
-            </div>
-            
-        </div>
-    </div>
-    <div class="owl-nav disabled">
-        <button type="button" role="presentation" class="owl-prev"><span aria-label="Previous">‹</span></button><button type="button" role="presentation" class="owl-next"><span aria-label="Next">›</span></button>
-    </div>
-    <div class="owl-dots disabled"></div>
-</div>
+<!-- .container END -->
+
 @endsection
--- resources/views/front/programas/ninos.blade.php
+++ resources/views/front/programas/ninos.blade.php
@@ -16,68 +16,16 @@ Entrenamiento kids
         </div>
     </div>
 </section>
-<section class="xs-black-white xs-section-padding-xs">
+</br>
+</br>
 
-    <section class="xs-section-padding" data-scrollax-parent="true">
-        <div class="container">
-            <div class="xs-help xs-strength">
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="xs-video-wraper">
-                            <img src="resources/img/index/ninos/foto-ninos.png" alt="" />
-                            
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        </br>
-                        </br>
-                        </br>
-                        <div class="xs-pl-15 xs-pr-15">
-                            <div class="xs-section-heading text-left">
-                                <h2>
-                                    Entrenamiento kids
-                                </h2>
-                            </div>
-                                <p>Por unos niños mas sanos y más felices</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <!-- .xs-help END -->
-        </div>
-        <!-- .container END -->
-    </section>
-</section>
-<section
-    class="xs-section-padding-lg xs-bg-cover parallaxie"
-    style=" background-image: url('resources/img/index/ninos/foto-frase.png'); background-size: cover; background-repeat: no-repeat; background-attachment: fixed; background-position: center 77.32px;"
->
-    <div class="container">
-        <div class="xs-help xs-help-light">
-            <div class="row">
-                <div class="col-lg-6"style="color:#fff;">
-                    <div class="xs-section-heading">
-                        <h2 style="color:#fff;">
-                            Este espacio fue creado<br />
-                            para el engreido de casa
-                        </h2>
-                    </div>
-                    <p>El ejercicio les ayuda a los niños a desarrollarse física y mentalmente, a estar sanos, y a relacionarse de una forma saludable con otros niños</p>
-                    
-                </div>
-            </div>
-        </div>
-        <!-- .xs-help END -->
-    </div>
-    <!-- .container END -->
-</section>
-<section class="xs-section-padding">
+<section class="xs-pt-xs xs-pb-s">
     <div class="container">
         <div class="row">
             <div class="col-lg-8 mx-auto">
                 <div class="xs-section-heading text-center">
-                    <h2>Beneficios</h2>
-                    <p>El entrenamieto fisico no sólo es bueno para la salud física del niño, también lo es para su salud mental; les ayudara tener más confianza en sí mismo, a relacionarse mejor con los demás e incluso a superar alguna enfermedad.</p>
+                    <h2 style="color:#0083AD;">Importancia</h2>
+                    <p>El ejercicio les ayuda a los niños a desarrollarse física y mentalmente, a estar sanos, y a relacionarse de una forma saludable con otros niños.</p>
                 </div>
             </div>
         </div>
@@ -86,6 +34,50 @@ Entrenamiento kids
     <!-- .container END -->
 </section>
 
+<section class="xs-pt-xs xs-pb-sm">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <div class="xs-team">
+                    <div>
+                        <img src="resources/img/index/maternidad/madre2.png" alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <div class="xs-team">
+                    <div>
+                        <img src="resources/img/index/maternidad/mama2.png" alt="" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</section>
+<!-- .container END -->
+</br>
+<section style="background-color:#0083AD;">
+    <div class="container">
+        <div>
+            <div class="row">
+                <div class="col-lg-8 mx-auto">
+                    </br>
+                    </br>
+                    </br>
+                    <div class="xs-section-heading text-center"style="color:#ffffff">
+                        <h2 style="color:#ffffff">Beneficios</h2>
+                        <p>La gimnasia laboral o también conocida como pausas activas, consiste en la ejecución de ejercicios físicos correctamente estructurados que actúan de manera terapéutica y preventiva, propiciando el bienestar general de sus trabajadores.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+</br>
+</br>
+</br>
+</br>
 <div class="owl-carousel owl-theme xs-service-info owl-loaded owl-drag">
     <div class="owl-stage-outer">
         <div class="owl-stage" style="transform: translate3d(-1801px, 0px, 0px); transition: all 0.25s ease 0s; width: 3089px;">
